1c4d6b7fdd71a7b515ccb2ccf5b493b0219692f8
ethantabbert <etabbert@vermeer.com>
2024-10-29 09:08:05 -0500

    make it work again

--- android/src/main/kotlin/com/xcellapps/flutter/rtsp_ffmpeg/FFMpegView.kt
+++ android/src/main/kotlin/com/xcellapps/flutter/rtsp_ffmpeg/FFMpegView.kt
@@ -11,26 +11,30 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.platform.PlatformView
 
-class FFMpegView internal constructor(context: Context?, messenger: BinaryMessenger, id: Int): PlatformView, MethodChannel.MethodCallHandler{
+class FFMpegView internal constructor(
+    context: Context?,
+    messenger: BinaryMessenger,
+    id: Int
+) : PlatformView, MethodChannel.MethodCallHandler {
 
     private val playerView = SurfaceView(context)
     private val methodChannel: MethodChannel
-    private val rtspClient = RtspClient(NativeCallback { frame, nChannel, width, height ->})
+    private val rtspClient = RtspClient(NativeCallback { frame, nChannel, width, height -> })
+
     init {
         playerView.holder.addCallback(object : SurfaceHolder.Callback {
-            override fun surfaceChanged(p0: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {
-                p0?.let {
-                    rtspClient.setHolder(it.surface, p1, p2)
-                }
+            override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+                rtspClient.setHolder(holder.surface, format, width)
             }
 
-            override fun surfaceDestroyed(p0: SurfaceHolder?) {
+            override fun surfaceDestroyed(holder: SurfaceHolder) {
             }
 
-            override fun surfaceCreated(p0: SurfaceHolder?) {
+            override fun surfaceCreated(holder: SurfaceHolder) {
             }
-
         })
+
+        
         methodChannel = MethodChannel(messenger, "rtsp_ffmpeg$id")
         methodChannel.setMethodCallHandler(this)
     }
@@ -44,7 +48,7 @@ class FFMpegView internal constructor(context: Context?, messenger: BinaryMessen
     }
 
     override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
-        when(call.method) {
+        when (call.method) {
             "play" -> onPlay(call.arguments as String, result)
             "stop" -> onStop(result)
             else -> result.notImplemented()
@@ -56,12 +60,12 @@ class FFMpegView internal constructor(context: Context?, messenger: BinaryMessen
         result.success(true)
     }
 
-    private fun onPlay(s: String, result: MethodChannel.Result) {
-        val res = rtspClient.play(s) == 0
-        if(res) {
-            result.success(res)
-        }else{
-            result.error("$res", "Error opening URL", "Error opening url")
+    private fun onPlay(url: String, result: MethodChannel.Result) {
+        val success = rtspClient.play(url) == 0
+        if (success) {
+            result.success(success)
+        } else {
+            result.error("ERROR_OPENING_URL", "Error opening URL", "Failed to open URL: $url")
         }
     }
 }
